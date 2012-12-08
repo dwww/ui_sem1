6905d686f59e1d4977ec1c353eed512b3168c746
Jan Vatovec <jan.vatovec@gmail.com>
2012-12-08 22:36:47 +0100

    Popravil data.py
    - razdelil podatke po vsebini
    - uredil podatke po letnicah
    - pobrisal staro get funkcijo
    - dodal nove funkcije za branje podatkov

--- data.py
+++ data.py
@@ -1,51 +1,112 @@
 """
 data loader
 """
-from matplotlib.cbook import tostr
 
-statistika = """
+standings = """
 fivb_world_championship_standings.txt
-fivb_world_cup_head_to_head_21-Oct-2011.txt
 fivb_world_cup_standings.txt
-fivb_world_league_head_to_head_03-Jun-2010.txt
-fivb_world_league_head_to_head_16-May-2012.txt
-fivb_world_league_head_to_head_27-May-2011.txt
 fivb_world_league_standings.txt
+""".strip().split("\n")
+
+head = """
+fivb_world_league_head_to_head_03-Jun-2010.txt
+fivb_world_league_head_to_head_27-May-2011.txt
+fivb_world_cup_head_to_head_21-Oct-2011.txt
+fivb_world_league_head_to_head_16-May-2012.txt
+""".strip().split("\n")
+
+statistics = """
 fivb_world_league_statistics_03-Jun-2010.txt
-fivb_world_league_statistics_16-May-2012.txt
 fivb_world_league_statistics_27-May-2011.txt
+fivb_world_league_statistics_16-May-2012.txt
+""".strip().split("\n")
+
+ranking = """
+fivb_world_ranking_15-Jan-2010.txt
+fivb_world_ranking_27-Jul-2010.txt
+fivb_world_ranking_15-Jan-2011.txt
 fivb_world_ranking_02-Oct-2011.txt
 fivb_world_ranking_04-Jan-2012.txt
-fivb_world_ranking_15-Jan-2010.txt
-fivb_world_ranking_15-Jan-2011.txt
-fivb_world_ranking_27-Jul-2010.txt
 """.strip().split("\n")
 
 tekme = """
 fivb_world_championship_2010.txt
 fivb_world_league_2010.txt
 fivb_world_league_2011.txt
-fivb_world_league_2012.txt
 fivb_world_cup_2011.txt
+fivb_world_league_2012.txt
 """.strip().split("\n")
 
 
+def presipaj(fun, ind):
+    tab = fun
+    for i in tab:
+        for j in i:
+            tmp = []
+            for n in ind:
+                tmp = tmp + [j[n]] 
+            j = tmp
+            print j
+
+def vrniStand():
+    
+    stand = [[i.strip().split(" ") for i in open("data/"+ime).readlines()] for ime in standings]
+        
+    for i in range(len(stand)): #s pomikanjem po tabeli se teza podatkov niza
+        stand[i] = stand[i][1:len(stand)]
+        for j in range(len(stand[i])):
+            for n in range(len(stand[i][j])):
+                if "-" in stand[i][j][n]: stand[i][j][n] = "-1" #iz - spremeni v -1 
+            print stand[i][j]
+    return stand
 
 
-#def getStatistika():
-#   return {ime : [i.strip().split("\t") for i in open("data/"+ime).readlines()] for ime in imenaDatotek}
+def vrniHead():
+    
+    heads = [[i.strip().split(" ") for i in open("data/"+ime).readlines()] for ime in head]
 
-stat = [[i.strip().split("\t") for i in open("data/"+ime).readlines()] for ime in tekme]
-stat = [[[n for n in j] for j in i] for i in stat]
-#Sstat = [tostr(i).contains("-"): [j for j in tostr(i).split("-")] for i in stat]
-for i in stat:
-    for j in i:
-        print j   
-    print "\n"
+    for i in heads:
+        i = i[1:len(i)]
+        for j in i:
+            print j
+    return heads
 
+
+def vrniRank():
+
+    rank = [[i.strip().split("\t") for i in open("data/"+ime).readlines()] for ime in ranking]
         
-        
-        
-        
-        
-        
+    for i in rank:
+        i = i[1:len(i)]
+        for j in i:
+            print j
+    return rank
+
+
+def vrniStat():
+   
+    stat = [[i.strip().split(" ") for i in open("data/"+ime).readlines()] for ime in statistics]
+    
+    for i in stat:
+        i = i[1:len(i)]
+        for j in i:
+            print j
+    return stat
+
+
+def vrniTekme():
+
+    pod = [[i.strip().split("\t") for i in open("data/"+ime).readlines()] for ime in tekme]
+    
+    for i in range(len(pod)):
+        pod[i] = pod[i][1:len(pod[i])]
+        for j in range(len(pod[i])):
+            for n in range(len(pod[i][j])):
+                if "-" in pod[i][j][n]: pod[i][j][n] = pod[i][j][n].split("-")
+                if "NA" in pod[i][j][n]: pod[i][j][n] = ["0","0"] #namesto rezultata NA da 0 : 0  
+            
+            pod[i][j] = pod[i][j][3:11] 
+            print pod[i][j]
+    return pod
+
+#presipaj(vrniStand(), range(5))

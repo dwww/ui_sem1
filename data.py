b20fe28cf4083099842c7f3971051f6dd7b43e28
Jan Vatovec <jan.vatovec@gmail.com>
2012-12-09 16:26:40 +0100

    Dodal metode za branje podatkov
    - dodal metode ki iz datotek parsajo podatke v slovarje

--- data.py
+++ data.py
@@ -2,106 +2,153 @@
 data loader
 """
 
-standings = """
-fivb_world_championship_standings.txt
-fivb_world_cup_standings.txt
-fivb_world_league_standings.txt
-""".strip().split("\n")
+from collections import defaultdict
 
-head = """
-fivb_world_league_head_to_head_03-Jun-2010.txt
-fivb_world_league_head_to_head_27-May-2011.txt
-fivb_world_cup_head_to_head_21-Oct-2011.txt
-fivb_world_league_head_to_head_16-May-2012.txt
-""".strip().split("\n")
+def split(line):
+    while line.find("  ") != -1:
+        line = line.replace("  "," ")
+    return line.replace("\t"," ").strip().split(" ")
 
-statistics = """
-fivb_world_league_statistics_03-Jun-2010.txt
-fivb_world_league_statistics_27-May-2011.txt
-fivb_world_league_statistics_16-May-2012.txt
-""".strip().split("\n")
-
-ranking = """
-fivb_world_ranking_15-Jan-2010.txt
-fivb_world_ranking_27-Jul-2010.txt
-fivb_world_ranking_15-Jan-2011.txt
-fivb_world_ranking_02-Oct-2011.txt
-fivb_world_ranking_04-Jan-2012.txt
-""".strip().split("\n")
-
-tekme = """
-fivb_world_championship_2010.txt
-fivb_world_league_2010.txt
-fivb_world_league_2011.txt
-fivb_world_cup_2011.txt
-fivb_world_league_2012.txt
-""".strip().split("\n")
-
-
-def presipaj(fun, ind):
-    tab = fun
-    for i in tab:
-        for j in i:
-            tmp = []
-            for n in ind:
-                tmp = tmp + [j[n]] 
-            j = tmp
-            print j
-
-def vrniStand():
+def vrniDatum(datum):
+    mes = {"Jan" : "01", "Feb" : "02", "Mar" : "03", "Apr" : "04", "May" : "05", "Jun" : "06", "Jul" : "07", "Aug" : "08", "Sep" : "09", "Oct" : "10", "Nov" : "11", "Dec" : "12"}
+    datum = datum.split("-")
+    return int(datum[2] + mes[datum[1]] + datum[0])
     
-    stand = [[i.strip().split(" ") for i in open("data/"+ime).readlines()[1:]] for ime in standings]
-        
-    for i in range(len(stand)): #s pomikanjem po tabeli se teza podatkov niza
-        for j in range(len(stand[i])):
-            print stand[i][j]
-    return stand
+def vrniStand():
+    standings = """
+    fivb_world_championship_standings.txt
+    fivb_world_cup_standings.txt
+    fivb_world_league_standings.txt
+    """.strip().split("\n")
+    result = {}
+    for ime in standings:
+        ime = ime.strip()
+        f = open("data/"+ime)
+        dIme = ime[11:-14] #.replace("fivb_world_","").replace("_standings.txt","")
+        head = split(f.readline())
+        stand = [split(i) for i in f.readlines()]
+        country = {}
+        for i,vrstica in enumerate(stand):
+            d = {int(head[j]+"1231") : vrstica[j] for j in range(2, len(vrstica))}
+            d["N"] = vrstica[1]
+            country[vrstica[0]] = d
+        result[dIme] = country
+    return result
 
 
 def vrniHead():
-    
-    heads = [[i.strip().split(" ") for i in open("data/"+ime).readlines()] for ime in head]
+    filenames = """
+    fivb_world_league_head_to_head_03-Jun-2010.txt
+    fivb_world_league_head_to_head_27-May-2011.txt
+    fivb_world_cup_head_to_head_21-Oct-2011.txt
+    fivb_world_league_head_to_head_16-May-2012.txt
+    """.strip().split("\n")
 
-    for i in heads:
-        i = i[1:len(i)]
-        for j in i:
-            print j
-    return heads
+    result = defaultdict(lambda : defaultdict(dict))
+
+    for file in filenames:
+        ime = file.strip() 
+        f = open("data/"+ime)
+        datum = vrniDatum(ime[-15:-4])        
+        glava = split(f.readline())[1:]
+        lines = [split(i) for i in f.readlines()]
+        for line in lines:
+            for i, element in enumerate(line[1:]):
+                result[line[0]][datum][glava[i]] = element
+            
+    return result
+
 
 
 def vrniRank():
+    ranking = """
+    fivb_world_ranking_15-Jan-2010.txt
+    fivb_world_ranking_27-Jul-2010.txt
+    fivb_world_ranking_15-Jan-2011.txt
+    fivb_world_ranking_02-Oct-2011.txt
+    fivb_world_ranking_04-Jan-2012.txt
+    """.strip().split("\n")
+    
+    result = defaultdict(dict)
+    
+    for ime in ranking:
+        ime = ime.strip()
+        f = open("data/"+ime)
+        datum = vrniDatum(ime[-15:-4])
+        rank = [split(i) for i in f.readlines()][1:]
+        for vrstica in rank:
+            result[vrstica[1]][datum] = {"Points" : vrstica[2], "Rank" : vrstica[0]}
 
-    rank = [[i.strip().split("\t") for i in open("data/"+ime).readlines()] for ime in ranking]
-        
-    for i in rank:
-        i = i[1:len(i)]
-        for j in i:
-            print j
-    return rank
+    return result
 
 
 def vrniStat():
-   
-    stat = [[i.strip().split(" ") for i in open("data/"+ime).readlines()] for ime in statistics]
+    statistics = """
+    fivb_world_league_statistics_03-Jun-2010.txt
+    fivb_world_league_statistics_27-May-2011.txt
+    fivb_world_league_statistics_16-May-2012.txt
+    """.strip().split("\n")
     
-    for i in stat:
-        i = i[1:len(i)]
-        for j in i:
-            print j
-    return stat
+    result = defaultdict(lambda : defaultdict(dict))
+    
+    for ime in statistics:
+        ime = ime.strip()
+        f = open("data/"+ime)
+        datum = vrniDatum(ime[-15:-4])
+        glava = split(f.readline())[1:]
+        lines = [split(i) for i in f.readlines()]
+        for line in lines:
+            for i, element in enumerate(line[1:]):
+                result[line[0]][datum][glava[i]] = element
+    return result
 
 
 def vrniTekme():
-
-    vseTekme = [[i.strip().split("\t") for i in open("data/"+ime).readlines()[1:]] for ime in tekme]
+    tekme = """
+    fivb_world_championship_2010.txt
+    fivb_world_league_2010.txt
+    fivb_world_league_2011.txt
+    fivb_world_cup_2011.txt
+    fivb_world_league_2012.txt
+    """.strip().split("\n")
     
-    for tekma in vseTekme:
-        for vrstica in tekma:
-            for i in range(len(vrstica)):
-                vrstica[i] = vrstica[i].split("-")
-                if "NA" in vrstica[i]: 
-                    vrstica[i] = ["0","0"] #namesto rezultata NA da 0 : 0  
-            print vrstica
-    return vseTekme
+    result = defaultdict(list)
+    
+    for ime in tekme:
+        ime = ime.strip()
+        f = open("data/"+ime)
+        imeT = ime.replace("fivb_world_","").split("_")[0]
+        glava = split(f.readline())
+        lines = [split(i) for i in f.readlines()]
+        for line in lines:
+            line[2] = vrniDatum(line[2])
+            result[imeT].append({glava[i] : el for i, el in enumerate(line)})
+    
+    return result
 
-#presipaj(vrniStand(), range(5))
+tekme = vrniTekme()
+stat = vrniStat()
+rank = vrniRank()
+head = vrniHead()
+stand = vrniStand()
+
+#def urediTekmo(tekma):
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
